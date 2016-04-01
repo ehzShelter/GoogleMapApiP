93a29ed002de0acf9e64f24d0488eed72379a1d1
ehzShelter <emrulhasanzawad@outlook.com>
2016-04-02 04:12:57 +0600

    google map api is cool

---
+++ map.js
@@ -0,0 +1,36 @@
+
+var firebase = new Firebase("https://demomapehz.firebaseio.com");
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    // latitude and longitude
+    center: {lat: 23.8103, lng: 90.4125},
+    zoom: 3
+  });
+
+  // Add marker on user click
+  map.addListener('click', function(e) {
+    firebase.push({lat: e.latLng.lat(), lng: e.latLng.lng()});
+  });
+
+  // Create a heatmap.
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: [],
+    map: map,
+    radius: 8
+  });
+
+  firebase.on("child_added", function(snapshot, prevChildKey) {
+    // Get latitude and longitude from Firebase.
+    var newPosition = snapshot.val();
+
+    // Create a google.maps.LatLng object for the position of the marker.
+    // A LatLng object literal (as above) could be used, but the heatmap
+    // in the next step requires a google.maps.LatLng object.
+    var latLng = new google.maps.LatLng(newPosition.lat, newPosition.lng);
+
+    heatmap.getData().push(latLng);
+  });
+}
+
+
